6a15142182f26efdcae21dc45b9f25af0c503a24
rodrigovidal <rodrigovidal@intranet>
2021-05-20 11:21:08 -0300

    Projeto Iniciado

---
+++ src/test/java/com/rodrigovidal/logisticaapi/LogisticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigovidal.logisticaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
